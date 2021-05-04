d035e7ffd3d765fce107aed13f5ec5624f5f4499
Gerardo Galván Chávez <38229183+arobtth136@users.noreply.github.com>
2021-05-04 12:23:57 -0500

    Base de datos
    Script y imagen diagrama

---
+++ models/reddit_clone.sql
@@ -0,0 +1,52 @@
+create database reddit_clone;
+
+use reddit_clone;
+
+create table usuarios(
+    id int not null unique auto_increment,
+    nombreUsuario varchar(64) not null unique ,
+    password blob not null ,
+    fechaNacimiento date not null ,
+    fotoPerfil varchar(128),
+    correo varchar(255),
+    PRIMARY KEY (id)
+);
+
+create table comunidades(
+    id int not null  unique auto_increment,
+    fotoGrupo varchar(128),
+    PRIMARY KEY (id)
+);
+
+create table posts(
+    id int not null unique auto_increment,
+    fechaPost date,
+    imagenes json,
+    texto text,
+    usuario int not null,
+    comunidad int not null,
+    likes int not  null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario) references usuarios(id),
+    FOREIGN KEY (comunidad) REFERENCES comunidades(id)
+);
+
+create table comentarios(
+    id int not null unique auto_increment,
+    texto text,
+    post int not null ,
+    usuario int not null,
+    comunidad int not null,
+    likes int not null,
+    respuesta int null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario) references usuarios(id),
+    FOREIGN KEY (comunidad) REFERENCES comunidades(id)
+);
+
+create table usuariosEnComunidad(
+    usuario int not null,
+    comunidad int not null,
+    FOREIGN KEY (usuario) references usuarios(id),
+    FOREIGN KEY (comunidad) references comunidades(id)
+);
